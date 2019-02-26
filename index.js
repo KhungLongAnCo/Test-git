a26373b2ec130b45f59377cd31fe36a76ad82963
Your Name <you@example.com>
2019-02-26 21:25:26 +0700

    push file cat eat dog

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 var dog = require('./moduledog.js');
-
-dog();
+var cat = function(){
+	this.eat = function(name){
+		console.log("my cat eat", name);
+	}
+}
+var dog = function(){
+	this.name = "skubido";
+}
+var jerry = new cat();
+var subido = new dog();
+jerry.eat(subido.name);
